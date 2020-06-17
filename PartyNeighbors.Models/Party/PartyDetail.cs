bc33b26355d93523b2c4a143037c88e1a7d171fd
Michael Milroy <mdmilroy@gmail.com>
2020-06-16 21:42:13 -0400

    added models for Party

--- PartyNeighbors.Models/Party/PartyDetail.cs
+++ PartyNeighbors.Models/Party/PartyDetail.cs
@@ -1,30 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PartyNeighbors.Data
+namespace PartyNeighbors.Models.Party
 {
-    public class Party
+    public class PartyDetail
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public int NeighborhoodId { get; set; }
-        [Required]
         public int LocationId { get; set; }
-        [Required]
         public DateTimeOffset PartyTime { get; set; }
-        [Required]
         public int HostId { get; set; }
-        [Required]
         public int Capacity { get; set; }
-        public List<string> Guests { get; set; } // populate with Resident's Full Name
+        public List<string> Guests { get; set; }
         public int PartyItemId { get; set; }
-        public List<PartyItem> PartyItems { get; set; }
+        public Dictionary<int, string> PartyItems { get; set; }
     }
 }
