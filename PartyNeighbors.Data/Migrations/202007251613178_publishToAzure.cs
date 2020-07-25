a909d7e26f5a72bf12b969f2ece33e2ac2d2cb14
Michael Milroy <michaeldmilroy@gmail.com>
2020-07-25 12:15:29 -0400

    tweaks to publish to Azure

---
+++ PartyNeighbors.Data/Migrations/202007251613178_publishToAzure.cs
@@ -0,0 +1,26 @@
+﻿namespace PartyNeighbors.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class publishToAzure : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PartyRSVP",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        GuestId = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PartyRSVP");
+        }
+    }
+}
